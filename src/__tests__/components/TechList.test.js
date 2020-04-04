cb5f713b663c22ee8fe3ec07d7256a724f18456b
Julio Henrique <juliohenrique@Julios-MacBook-Air.local>
2020-04-04 19:06:52 -0300

    increment first test with submit on form

--- src/__tests__/components/TechList.test.js
+++ src/__tests__/components/TechList.test.js
@@ -5,8 +5,12 @@ import TechList from '../../components/TechList';
 
 describe('TechList component', () => {
   it('should be able to add new tech', () => {
-    const { getByText, getByTestId } = render(<TechList />);
-    fireEvent.click(getByText('Adicionar'));
+    const { getByText, getByTestId, getByLabelText } = render(<TechList />);
+
+    fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js'} });
+    fireEvent.submit(getByTestId('tech-form'));
+
     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+    expect(getByLabelText('Tech')).toHaveValue('')
   })
 })
--- src/components/TechList/index.js
+++ src/components/TechList/index.js
@@ -2,16 +2,22 @@ import React, { useState } from 'react'
 
 export default function TechList() {
   const [techs, setTechs] = useState([]);
+  const [newTech, setNewTech] = useState('');
 
   function handleAddTech(){
-    setTechs([...techs, 'Node.js'])
+    setTechs([...techs, newTech]);
+    setNewTech("");
   }
   return (
-    <div>
+    <form data-testid="tech-form" onSubmit={handleAddTech}>
       <ul data-testid="tech-list">
         {techs.map(tech => <li key={tech}>{tech}</li>)}
-        <button onClick={handleAddTech}>Adicionar</button>
       </ul>
-    </div>
+
+      <label htmlFor="tech">Tech</label>
+      <input id="tech" value={newTech} onChange={e => setNewTech(e.target.value)} type="text" />
+
+      <button onClick={handleAddTech}>Adicionar</button>
+    </form>
   )
 }
