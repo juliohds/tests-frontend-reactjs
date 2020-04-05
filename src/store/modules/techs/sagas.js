e0ffa3807df144a69e62496b6f61c6b22cc63ef2
Julio Henrique <juliohenrique@Julios-MacBook-Air.local>
2020-04-04 21:32:56 -0300

    testing api with mocks

--- src/store/modules/techs/sagas.js
+++ src/store/modules/techs/sagas.js
@@ -1,4 +1,4 @@
-import { call, put } from 'redux-saga';
+import { call, put } from 'redux-saga/effects';
 
 import api from '../../../services/api';
 
@@ -7,7 +7,7 @@ import { getTechsSuccess, getTechsFailure } from './actions';
 export function* getTechs() {
   try {
     const response = yield call(api.get, 'techs');
-
+    console.log(response.data)
     yield put(getTechsSuccess(response.data));
   } catch (err) {
     yield put(getTechsFailure());
